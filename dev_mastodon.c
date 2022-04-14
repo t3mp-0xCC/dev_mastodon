1c29d60611b38125597832e8510d25d8aaa1536f
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-04-15 02:36:52 +0900

    temp commit

--- dev_mastodon.c
+++ dev_mastodon.c
@@ -7,6 +7,9 @@
 #include <linux/sched.h>
 #include <linux/device.h>
 #include <linux/slab.h>
+#include <asm/current.h>
+#include <asm/uaccess.h>
+#include <linux/kmod.h>
 
 MODULE_LICENSE("MIT");
 MODULE_DESCRIPTION("toot from /dev/mastodon");
@@ -36,7 +39,7 @@ static int toot(char *text) {
 }
 
 static int open(struct inode *inode, struct file *file) {
-  struct toot_buffer *toot_buf;
+                struct toot_buffer *toot_buf;
 
   toot_buf = kmalloc(sizeof(struct toot_buffer), GFP_KERNEL);
   if (toot_buf == NULL) {
@@ -61,6 +64,75 @@ static int release(struct inode *inode, struct file *file) {
     file->private_data = NULL;
   }
   return 0;
+}
+
+static ssize_t dummy_read(struct file *file, char __user *buf, size_t count,
+                          loff_t *f_pos) {
+  // /dev/mastodon is not readable device...
+  return 0;
+}
+
+static ssize_t write(struct file *file, const char __user *buf, size_t count,
+                     loff_t *f_pos) {
+  struct toot_buffer *toot_buf;
+  unsigned int read_size;
+  unsigned int text_pointer, text_counter;
+  char char_buf;
+  unsigned int processed_count;
+
+  toot_buf = file->private_data;
+  if (toot_buf) {
+    processed_count = 0;
+    while (count > processed_count) {
+      if (count > TOOT_BUFFER_SIZE - toot_buf->pointer) {
+        read_size = TOOT_BUFFER_SIZE - toot_buf->pointer;
+      } else {
+        read_size = count;
+      }
+      // read data from user process
+      if (raw_copy_from_user(toot_buf->buffer + toot_buf->pointer,
+                             buf + processed_count, read_size) != 0) {
+      printk(KERN_WARNING "Cannot copy data from buffer\n");
+      return -EFAULT;
+      }
+      toot_buf->buffer[toot_buf->pointer + read_size] = '\0';
+      for (text_counter = 0, text_pointer = 0;
+           text_pointer < toot_buf->pointer + read_size;
+           text_pointer++) {
+        if(!((unsigned char)toot_buf->buffer[text_pointer] >= 0x80 &&
+           0xBF >= (unsigned)toot_buf->buffer[text_pointer])) {
+          text_counter++;
+          if(text_counter > MAX_TOOT_LENGTH) {
+            char_buf = toot_buf->buffer[text_pointer];
+            toot_buf->buffer[text_pointer] = '\0';
+            if(toot(toot_buf->buffer)) {
+              return -EFAULT;
+            }
+            toot_buf->buffer[text_pointer] = char_buf;
+            break;
+          }
+        }
+      }
+      if (text_counter == MAX_TOOT_LENGTH) {
+        if (toot_buf->buffer) {
+          return -EFAULT;
+        }
+        toot_buf->pointer = 0;
+      } else if (text_counter > MAX_TOOT_LENGTH) {
+        memcpy(toot_buf->buffer, toot_buf->buffer + text_pointer,
+               toot_buf->pointer + read_size - text_pointer);
+        toot_buf->pointer =
+          toot_buf->pointer + read_size - text_pointer;
+      } else {
+        toot_buf->pointer += read_size;
+      }
+      processed_count += read_size;
+    }
+  } else {
+    printk(KERN_WARNING "Cannot get toot buffer\n");
+    return -EFAULT;
+  }
+  return count;
 }
 
 struct file_operations fops = {
