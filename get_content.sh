419fad087239d54fdc03907c1114b6212d5a8ba6
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-04-18 05:08:07 +0900

    update get_content.sh

--- get_content.sh
+++ get_content.sh
@@ -8,4 +8,4 @@ url="https://$INSTANCE/api/v1/timelines/public"
 
 curl $url?limit=40 2>/dev/null | jq '. | map(.content)' \
 | tr -d -d "\n" | tr -d -d "<p>" | tr -d -d "</p>" \
-#| tr -d -d "[" | tr -d -d "]" | tr -d -d ", " | tr -d -d "\"" | tr -d -d "br"
+| tr -d -d "[" | tr -d -d "]" | tr -d -d ", " | tr -d -d "\"" | tr -d -d "br"
